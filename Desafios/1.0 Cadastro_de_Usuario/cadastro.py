11cce441d6f5e9d3e34d74afd6d29ece4c923109
marcosVScarvalho <marcos.carvalho1@iesb.edu.br>
2025-05-21 11:07:59 -0300

    Update cadastro.py

--- Desafios/1.0 Cadastro_de_Usuario/cadastro.py
+++ Desafios/1.0 Cadastro_de_Usuario/cadastro.py
@@ -1,4 +1,4 @@
-class Usuario():
+class Usuario:
     def __init__(self,nome,email,idade):
         self.nome = nome
         self.email = email
@@ -7,15 +7,16 @@ class Usuario():
     def __str__(self):
         return f'{self.nome} | {self.email} | {self.idade}'
     
-class SistemaCadastro():
+class SistemaCadastro:
     usuarios = []
 
     def adicionar_usuario(self,usuario):
         self.usuarios.append(usuario)
     
-    def listar_usuarios(self):
+    @classmethod
+    def listar_usuarios(cls):
         print (f'{'Nome'.ljust(25)} | {'Email'.ljust(25)} | {'idade'}')
-        for usuario in SistemaCadastro.usuarios:
+        for usuario in cls.usuarios:
             print (f'{usuario.nome.ljust(25)} | {usuario.email.ljust(25)} | {usuario.idade} ')
 
 usuario = Usuario("Camila", "camila@email.com", 25)
